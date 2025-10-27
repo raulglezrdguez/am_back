fbf05e0a8aba7ff71444c683958c76efae67bc75
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-10-27 08:47:54 -0400

    server connected verification added

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "./index.ts";
 
-export const connectDB = async () =>
+export const connectDB = async () => {
+  if (mongoose.connection.readyState === 1) return; // ya conectado
+
   await mongoose.connect(MONGO_URI || "mongodb://127.0.0.1:27017/am_back");
+};
