ae0425d2919f5fe7f81cb35c86b7c86fb3b35ff1
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-10-27 08:55:09 -0400

    require by import changed

--- config/admin.firebase.ts
+++ config/admin.firebase.ts
@@ -1,6 +1,16 @@
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
+import { readFileSync } from "fs";
 
-const serviceAccount = require("../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json");
+// import serviceAccount from "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json" with { type: 'json' };
+const serviceAccount = JSON.parse(
+  readFileSync(
+    new URL(
+      "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json",
+      import.meta.url
+    ),
+    "utf-8"
+  )
+) as admin.ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
