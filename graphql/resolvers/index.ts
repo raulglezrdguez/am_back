8f15a536c2d640d57c2f67d681f0fbdfc4db81a6
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-10-31 11:53:48 -0400

    exam resolvers added

--- graphql/resolvers/index.ts
+++ graphql/resolvers/index.ts
@@ -2,6 +2,11 @@ import { mergeResolvers } from "@graphql-tools/merge";
 
 // import { scalars } from "../scalars.ts";
 import userResolvers from "./user.resolver.ts";
+import examResolvers from "./exam.resolver.ts";
 import patientResolvers from "./patient.resolver.ts";
 
-export const resolvers = mergeResolvers([userResolvers, patientResolvers]);
+export const resolvers = mergeResolvers([
+  userResolvers,
+  examResolvers,
+  patientResolvers,
+]);
