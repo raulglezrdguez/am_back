fe0ff92ec87abc2fff9070f5bfcba61d9489a2e6
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-11-05 07:48:32 -0500

    stat resolvers added

--- graphql/resolvers/index.ts
+++ graphql/resolvers/index.ts
@@ -4,9 +4,11 @@ import { mergeResolvers } from "@graphql-tools/merge";
 import userResolvers from "./user.resolver.ts";
 import examResolvers from "./exam.resolver.ts";
 import patientResolvers from "./patient.resolver.ts";
+import statResolvers from "./stat.resolver.ts";
 
 export const resolvers = mergeResolvers([
   userResolvers,
   examResolvers,
   patientResolvers,
+  statResolvers,
 ]);
