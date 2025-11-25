52292fb9729b9b6b6c10b0b23086df07c8dc8789
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-11-25 06:56:00 -0500

    firebase service account env variable added

--- config/admin.firebase.ts
+++ config/admin.firebase.ts
@@ -2,15 +2,33 @@ import admin from "firebase-admin";
 import { readFileSync } from "fs";
 
 // import serviceAccount from "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json" with { type: 'json' };
-const serviceAccount = JSON.parse(
-  readFileSync(
-    new URL(
-      "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json",
-      import.meta.url
-    ),
-    "utf-8"
-  )
-) as admin.ServiceAccount;
+// const serviceAccount = JSON.parse(
+//   readFileSync(
+//     new URL(
+//       "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json",
+//       import.meta.url
+//     ),
+//     "utf-8"
+//   )
+// ) as admin.ServiceAccount;
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  const serviceAccountJson = Buffer.from(
+    process.env.FIREBASE_SERVICE_ACCOUNT,
+    "base64"
+  ).toString("utf8");
+  serviceAccount = JSON.parse(serviceAccountJson);
+} else {
+  serviceAccount = JSON.parse(
+    readFileSync(
+      new URL(
+        "../.private/sharexam-f26e8-firebase-adminsdk-fbsvc-2baa2f0f48.json",
+        import.meta.url
+      ),
+      "utf-8"
+    )
+  ) as admin.ServiceAccount;
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
